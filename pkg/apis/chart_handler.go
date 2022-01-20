82c89da6e75320bf07560e1183fd72aa128953f3
Jeonghyeon Yang <yxzzzxh@gmail.com>
2022-01-20 16:05:21 +0900

    [feat] Implement getChartList

--- pkg/apis/chart_handler.go
+++ pkg/apis/chart_handler.go
@@ -1,9 +1,12 @@
 package apis
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/tmax-cloud/helm-apiserver/pkg/schemas"
 	"k8s.io/klog"
@@ -11,9 +14,63 @@ import (
 	helmclient "github.com/mittwald/go-helm-client"
 )
 
-// [TODO] : 구현
-func (hcm *HelmClientManager) GetCharts(w http.ResponseWriter, r *http.Request) {
+type Request struct {
+	ChartRepositoryName string
+}
 
+const (
+	indexFileSuffix  = "-index.yaml"
+	chartsFileSuffix = "-charts.txt"
+)
+
+// 설치 가능한 chart list 반환
+// AddChartRepo로 helm-charts 레포를 등록했다고 가정
+// helmcache에서 {chart-repo-name}-index.yaml
+// helmcache에서 {chart-repo-name}-charts.txt
+func (hcm *HelmClientManager) GetCharts(w http.ResponseWriter, r *http.Request) {
+	klog.Infoln("GetCharts")
+	//req := schemas.ChartRequest{}
+	req := Request{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		klog.Errorln(err, "failed to decode request")
+		return
+	}
+
+	charts, err := getChartList(req.ChartRepositoryName)
+	if err != nil {
+		klog.Errorln(err, "failed to get chart list")
+		return
+	}
+
+	for i, s := range charts {
+		fmt.Println(i, s)
+	}
+}
+
+func getChartList(ChartRepoName string) ([]string, error) {
+	// open the file
+	file, err := os.Open(repositoryCache + "/" + ChartRepoName + chartsFileSuffix)
+
+	defer file.Close()
+
+	//handle errors while opening
+	if err != nil {
+		klog.Errorln(err, "Error when opening file")
+		return nil, err
+	}
+
+	fileScanner := bufio.NewScanner(file)
+
+	var chartList []string
+
+	// read line by line
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		chartList = append(chartList, line)
+		fmt.Println(line)
+	}
+
+	return chartList, nil
 }
 
 func (hcm *HelmClientManager) InstallChart(w http.ResponseWriter, r *http.Request) {
